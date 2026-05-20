0f160f56b279d451fd732bd299f60cd90d01a736
Maciej Jan <maciej.mosio@op.pl>
2026-05-20 15:58:27 +0200

    Refactor DHT22 driver to use sensor structure

--- dht22_interrupt_driver.c
+++ dht22_interrupt_driver.c
@@ -1,49 +1,34 @@
 #include <dht22_interrupt_driver.h>
 #include "pin_mux.h"
+#include <string.h>
+#include "fsl_debug_console.h"
 
-
-static volatile bool measures_ready = false;
-
-static bool trigger_did = false;
-
-static bool trigger_delay = false;
-
-static volatile bool is_measuring = false;
-
-static volatile uint32_t response_start_time = 0;
-
-static volatile uint8_t bits_itter = 0;
-
-static uint32_t trigger_start_time = 0;
-
-static volatile uint8_t handshake_step = 0;
-
-static volatile uint8_t bit_tab[5] = {0};
 
 static const uint8_t BIT_MAP_TAB[8] = {0x80,0x40,0x20,0x10,0x08,0x04,0x02,0x01};
 
-void INT_0_IRQHANDLER(void){
-	if(GPIO_PinGetInterruptFlag(DHT22_GPIO, DHT22_PIN)){
-		GPIO_PinClearInterruptFlag(DHT22_GPIO, DHT22_PIN);
 
-		if(is_measuring && !measures_ready){
-			if(handshake_step < 3){
-				++handshake_step;
+void DHT22_Interrupt(DHT22_Sensor* dht22){
+	if(GPIO_PinGetInterruptFlag(dht22->base, dht22->pin)){
+		GPIO_PinClearInterruptFlag(dht22->base, dht22->pin);
+
+		if(dht22->is_measuring && !dht22->measures_ready){
+			if(dht22->handshake_step < 3){
+				++dht22->handshake_step;
 				return;
 			}
 
-			if(DHT22_GPIO->PDIR & DHT22_PIN_MASK){
-				response_start_time = CTIMER0->TC;
+			if(dht22->base->PDIR & (1U << dht22->pin)){
+				dht22->response_start_time = CTIMER0->TC;
 			} else{
-				if((CTIMER0->TC - response_start_time) > 30U){
-					 *(bit_tab + (bits_itter >> 3)) |= *(BIT_MAP_TAB + (bits_itter & 7));
+				if((CTIMER0->TC - dht22->response_start_time) > 30U){
+					 *(dht22->bit_tab + (dht22->bits_itter >> 3)) |= *(BIT_MAP_TAB + (dht22->bits_itter & 7));
 				}
 
-				++bits_itter;
+				++dht22->bits_itter;
 
-				if(bits_itter == 40){
-					is_measuring = false;
-					measures_ready = true;
+				if(dht22->bits_itter == 40){
+					dht22->is_measuring = false;
+					dht22->measures_ready = true;
 				}
 			}
 		}
@@ -51,55 +36,58 @@ void INT_0_IRQHANDLER(void){
 
 }
 
-static inline void DHT22_Reset_Flags(void){
-	measures_ready = false;
-	is_measuring = false;
-	trigger_did = false;
-	trigger_delay = false;
-	response_start_time = 0;
-	trigger_start_time = 0;
-	handshake_step = 0;
-	bits_itter = 0;
+static inline void DHT22_Reset_Flags(DHT22_Sensor* dht22){
+	dht22->measures_ready = false;
+	dht22->is_measuring = false;
+	dht22->trigger_did = false;
+	dht22->trigger_delay = false;
+	dht22->response_start_time = 0;
+	dht22->trigger_start_time = 0;
+	dht22->handshake_step = 0;
+	dht22->bits_itter = 0;
 }
 
 
-static void DHT22_Do_Trigger(void){
-	if(!trigger_delay){
+static void DHT22_Do_Trigger(DHT22_Sensor* dht22){
+	if(!dht22->trigger_delay){
 
-		trigger_delay = true;
+		dht22->trigger_delay = true;
 
-		DHT22_GPIO->PCOR = DHT22_PIN_MASK;
-		DHT22_GPIO->PDDR |= DHT22_PIN_MASK;
+		dht22->base->PCOR = (1U << dht22->pin);
+		dht22->base->PDDR |= (1U << dht22->pin);
 
-		trigger_start_time = CTIMER0->TC;
+		dht22->trigger_start_time = CTIMER0->TC;
 
 	}
 
 
-	if((CTIMER0->TC - trigger_start_time) >= 1000){
-		trigger_delay = false;
-		DHT22_GPIO->PDDR &= ~DHT22_PIN_MASK;
+	if((CTIMER0->TC - dht22->trigger_start_time) >= 1000){
+		dht22->trigger_delay = false;
+		dht22->base->PDDR &= ~(1U << dht22->pin);
 
 		delay_us(5U);
-		GPIO_PinClearInterruptFlag(DHT22_GPIO, DHT22_PIN);
+		GPIO_PinClearInterruptFlag(dht22->base, dht22->pin);
 
-		is_measuring = true;
-		trigger_did = true;
+		dht22->is_measuring = true;
+		dht22->trigger_did = true;
 	}
 }
 
-uint32_t DHT22_Get_Temperature_And_RH(void){
-	if(!trigger_did){
-		DHT22_Do_Trigger();
+
+
+
+uint32_t DHT22_Get_Temperature_And_RH(DHT22_Sensor* dht22){
+	if(!dht22->trigger_did){
+		DHT22_Do_Trigger(dht22);
 		return 0;
 	}
 
-	if(!measures_ready){
-		if((CTIMER0->TC - trigger_start_time) >= 20000U){
+	if(!dht22->measures_ready){
+		if((CTIMER0->TC - dht22->trigger_start_time) >= 20000U){
 
-			DHT22_Reset_Flags();
+			DHT22_Reset_Flags(dht22);
 
-			bit_tab[0] = 0; bit_tab[1] = 0; bit_tab[2] = 0; bit_tab[3] = 0; bit_tab[4] = 0;
+			dht22->bit_tab[0] = 0; dht22->bit_tab[1] = 0; dht22->bit_tab[2] = 0; dht22->bit_tab[3] = 0; dht22->bit_tab[4] = 0;
 
 			return CHECK_SUM_ERROR;
 		}
@@ -108,16 +96,55 @@ uint32_t DHT22_Get_Temperature_And_RH(void){
 
 	else {
 
-		DHT22_Reset_Flags();
+		DHT22_Reset_Flags(dht22);
 
 		uint32_t results = 0;
 
-		if((uint8_t)(*bit_tab + *(bit_tab + 1) + *(bit_tab + 2) + *(bit_tab + 3)) == *(bit_tab + 4)) {
-			results = ((uint32_t)(*(bit_tab)) << 24) | ((uint32_t)(*(bit_tab + 1)) << 16) | ((uint32_t)(*(bit_tab + 2)) << 8) | ((uint32_t)(*(bit_tab + 3)));
+		if((uint8_t)(*dht22->bit_tab + *(dht22->bit_tab + 1) + *(dht22->bit_tab + 2) + *(dht22->bit_tab + 3)) == *(dht22->bit_tab + 4)) {
+			results = ((uint32_t)(*(dht22->bit_tab)) << 24) | ((uint32_t)(*(dht22->bit_tab + 1)) << 16) | ((uint32_t)(*(dht22->bit_tab + 2)) << 8) | ((uint32_t)(*(dht22->bit_tab + 3)));
 		} else results = CHECK_SUM_ERROR;
 
-		bit_tab[0] = 0; bit_tab[1] = 0; bit_tab[2] = 0; bit_tab[3] = 0; bit_tab[4] = 0;
+		dht22->bit_tab[0] = 0; dht22->bit_tab[1] = 0; dht22->bit_tab[2] = 0; dht22->bit_tab[3] = 0; dht22->bit_tab[4] = 0;
 
 		return results;
 	}
+}
+
+
+
+void DHT22_Set_Structure(DHT22_Sensor* dht22, GPIO_Type* base, uint8_t pin){
+	memset(dht22, 0, sizeof(DHT22_Sensor));
+
+	dht22->base = base;
+	dht22->pin = pin;
+}
+
+
+
+
+void DHT22_Process_And_Print_Sensor_Data(uint32_t raw_data, const char* sensor_name) {
+    if (raw_data == CHECK_SUM_ERROR) {
+        PRINTF("[%s] ERROR: Checksum mismatch!\r\n", sensor_name);
+        return;
+    }
+
+    uint16_t raw_humidity = (uint16_t)((raw_data >> 16) & 0xFFFF);
+    uint16_t raw_temperature = (uint16_t)(raw_data & 0xFFFF);
+
+    uint8_t hum_integral = raw_humidity / 10;
+    uint8_t hum_fractional = raw_humidity % 10;
+
+    int8_t temp_integral = 0;
+    int8_t temp_fractional = 0;
+
+    if (raw_temperature & 0x8000) {
+        uint16_t absolute_temp = raw_temperature & 0x7FFF;
+        temp_integral = -(int8_t)(absolute_temp / 10);
+        temp_fractional = (int8_t)(absolute_temp % 10);
+    } else {
+        temp_integral = (int8_t)(raw_temperature / 10);
+        temp_fractional = (int8_t)(raw_temperature % 10);
+    }
+
+    PRINTF("[%s] RH: %u.%u%% | Temp: %d.%d st.C\r\n",sensor_name, hum_integral, hum_fractional, temp_integral, temp_fractional);
 }
