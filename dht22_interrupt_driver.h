8ad657a1b90d48be08f7f2ecef3d3f8114238e8d
Maciej Jan <maciej.mosio@op.pl>
2026-05-20 15:58:52 +0200

    Refactor DHT22 driver for improved structure and functionality

--- dht22_interrupt_driver.h
+++ dht22_interrupt_driver.h
@@ -5,13 +5,6 @@
  *      Author: silnymaciej
  */
 
-/*
-*
-*
-*  WORK IN PROGRESS
-*
-*/
-
 #ifndef DHT22_INTERRUPT_DRIVER_H_
 #define DHT22_INTERRUPT_DRIVER_H_
 
@@ -19,9 +12,34 @@
 #include "board.h"
 #include "delay_us.h"
 
-#define DHT22_GPIO 					BOARD_DHT22_INITPINS_dht22_GPIO
-#define DHT22_PIN_MASK 				BOARD_DHT22_INITPINS_dht22_GPIO_PIN_MASK
-#define DHT22_PIN					BOARD_DHT22_INITPINS_dht22_GPIO_PIN
+#define DHT22_1_GPIO 					BOARD_DHT22_INITPINS_dht22_GPIO
+#define DHT22_1_PIN					    BOARD_DHT22_INITPINS_dht22_GPIO_PIN
+
+
+typedef struct{
+	GPIO_Type* base;
+	uint8_t pin;
+
+	volatile bool measures_ready;
+
+	bool trigger_did;
+
+	bool trigger_delay;
+
+	volatile bool is_measuring;
+
+	volatile uint32_t response_start_time;
+
+	volatile uint8_t bits_itter;
+
+	uint32_t trigger_start_time;
+
+	volatile uint8_t handshake_step;
+
+	volatile uint8_t bit_tab[5];
+}DHT22_Sensor;
+
+
 
 typedef enum{
 	GND_ERROR = 112,
@@ -30,7 +48,9 @@ typedef enum{
 }g_Sensor_Check_t;
 
 
-uint32_t DHT22_Get_Temperature_And_RH(void);
-
+uint32_t DHT22_Get_Temperature_And_RH(DHT22_Sensor* dht22);
+void DHT22_Interrupt(DHT22_Sensor* dht22);
+void DHT22_Set_Structure(DHT22_Sensor* dht22, GPIO_Type* base, uint8_t pin);
+void DHT22_Process_And_Print_Sensor_Data(uint32_t raw_data, const char* sensor_name);
 
 #endif /* DHT22_INTERRUPT_DRIVER_H_ */
